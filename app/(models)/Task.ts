f84ed9a565a648fff88bc8844ec3e58ca30966d6
Tausif4171 <tausifkhan4171@gmail.com>
2024-05-12 19:13:22 +0530

    model schema created for our app

---
+++ app/(models)/Task.ts
@@ -0,0 +1,23 @@
+import mongoose, { Schema } from "mongoose";
+
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI);
+}
+
+const taskSchema = new Schema(
+  {
+    title: String,
+    description: String,
+    status: String,
+    progress: Number,
+    priority: Number,
+    category: String,
+    active: Boolean,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Task = mongoose.model("Task", taskSchema);
+export default Task;
