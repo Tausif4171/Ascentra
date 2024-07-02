9ef7a881a7cc93fa9c23cfa56a06571c6e7213dc
Tausif4171 <tausifkhan4171@gmail.com>
2024-07-02 22:51:50 +0530

    format date and time

--- app/(components)/TicketCard.tsx
+++ app/(components)/TicketCard.tsx
@@ -5,28 +5,55 @@ import ProgressDisplay from "./ProgressDisplay";
 import StatusDisplay from "./StatusDisplay";
 
 function TicketCard({ item, index }: any) {
-  console.log('check',item)
+  console.log('check', item)
+
+  function format( timestamp : any) {
+    const options: any = {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      hour12: true
+    }
+
+    const date = new Date(timestamp)
+    const formatDateTime = date.toLocaleString('en-US', options)
+    return formatDateTime
+
+  }
+
   return (
     <div className=" bg-white rounded-xl h-auto p-4 hover:bg-slate-50 cursor-pointer">
+
       <div className="flex justify-between">
         <PriorityDisplay priority={item.priority} />
         <DeleteBlock />
       </div>
+
       <h2 className="mt-2">{item.title}</h2>
+
       <hr className=" h-px text-sky-300 w-full"></hr>
+
       <p className="mt-2">{item.description}</p>
-      <p className="mt-2">1{item.createdAt}</p>
+
+      <p className="mt-2">{format(item.createdAt)}</p>
+
       <div
         className="flex justify-between w-full mt-2"
         style={{ alignItems: "center" }}
       >
+
         <div className="w-64">
           <ProgressDisplay progress={item.progress} />
         </div>
+
         <div className="">
           <StatusDisplay status={item.status} />
         </div>
+
       </div>
+
     </div>
   );
 }
