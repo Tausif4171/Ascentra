470b4555508d231643430b9815f8aae6feb0fedd
Tausif4171 <tausifkhan4171@gmail.com>
2024-07-02 12:24:19 +0530

    fetch in frontend

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,22 @@
 import TicketCard from "./(components)/TicketCard";
 
-export default function Home() {
+const getTask = async () => {
+  try {
+    const res = await fetch('http://localhost:3000/api/Task', { cache: 'no-store' });
+    const data = await res.json();
+    return data;
+  }
+  catch (error) {
+    console.error("Failed to fetch tasks:", error);
+    return { task: [] };
+  }
+}
+
+
+export default async function Home() {
+  const { task } = await getTask();
+  console.log({ task });
+
   return (
     <div className=" grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4  m-10">
       <TicketCard />
