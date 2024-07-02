14b34d901074868fc73a74f39bda4de9742aea50
Tausif4171 <tausifkhan4171@gmail.com>
2024-07-02 13:17:44 +0530

    fixed progress issue

--- app/(components)/ProgressDisplay.tsx
+++ app/(components)/ProgressDisplay.tsx
@@ -3,7 +3,7 @@ import React from "react";
 function ProgressDisplay({progress}:any) {
   return (
     <div className=" bg-slate-200 rounded-full w-full">
-      <div className={` bg-emerald-200 w-[${progress}%] rounded-full`}>
+      <div className={` bg-emerald-200 rounded-full`} style={{width:`${progress}%`}}>
         <div className="text-emerald-200">s</div>
       </div>
     </div>
