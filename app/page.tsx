a68c144455188c28ab7397fac97ac54b393b3f0c
Tausif4171 <tausifkhan4171@gmail.com>
2024-07-13 11:48:58 +0530

    handled undefined case

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,16 @@ import TaskSchema from "./interface/Task";
 
 const getTask = async () => {
   try {
-    const res = await fetch('http://localhost:3000/api/Task', { cache: 'no-store' });
+    const res = await fetch("http://localhost:3000/api/Task", {
+      cache: "no-store",
+    });
     const data = await res.json();
     return data;
-  }
-  catch (error) {
+  } catch (error) {
     console.error("Failed to fetch tasks:", error);
     return { task: [] };
   }
-}
-
+};
 
 export default async function Home() {
   const { task } = await getTask();
@@ -23,15 +23,22 @@ export default async function Home() {
       <h3>Software</h3>
 
       <div className=" grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4  m-10">
-        {task.filter((data: TaskSchema) => data.category === 'Software').map((item: TaskSchema, index: number) => <TicketCard item={item} index={index} />)}
+        {task
+          ?.filter((data: TaskSchema) => data.category === "Software")
+          .map((item: TaskSchema, index: number) => (
+            <TicketCard item={item} index={index} />
+          ))}
       </div>
 
       <h3>Hardware</h3>
 
       <div className=" grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4  m-10">
-        {task.filter((data: TaskSchema) => data.category === 'Hardware').map((item: TaskSchema, index: number) => <TicketCard item={item} index={index} />)}
+        {task
+          ?.filter((data: TaskSchema) => data.category === "Hardware")
+          .map((item: TaskSchema, index: number) => (
+            <TicketCard item={item} index={index} />
+          ))}
       </div>
     </div>
-
   );
 }
