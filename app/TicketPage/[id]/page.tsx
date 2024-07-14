c2e55403cfa8bb621b98f68962bee151d2f8599a
Tausif4171 <tausifkhan4171@gmail.com>
2024-07-14 18:28:49 +0530

    fixed issue of not getting data

--- app/TicketPage/[id]/page.tsx
+++ app/TicketPage/[id]/page.tsx
@@ -2,7 +2,7 @@ import { TaskForm } from "@/app/(components)/TaskForm";
 import React from "react";
 
 const getDataBasedOnId = async (id: number) => {
-  const res = await fetch(`/api/Task/${id}`, {
+  const res = await fetch(`http://localhost:3000/api/Task/${id}`, {
     cache: "no-store",
   });
   console.log({ res });
@@ -12,15 +12,17 @@ const getDataBasedOnId = async (id: number) => {
 };
 
 async function TicketPage({ params }: any) {
+  let data = {};
   console.log({ params });
   const editMode = params.id !== "new" ? true : false;
   if (editMode) {
     const ticketData = await getDataBasedOnId(params.id);
     console.log({ ticketData });
+    data = ticketData.taskData;
   }
   return (
     <div>
-      <TaskForm />
+      <TaskForm data={data} editMode={editMode} />
     </div>
   );
 }
