c4bb4ff848459e37214a92bcb6c5212511c3488c
Tausif4171 <tausifkhan4171@gmail.com>
2024-07-20 13:38:37 +0530

    connection added for vercel deployment routes

--- app/(models)/Task.ts
+++ app/(models)/Task.ts
@@ -1,9 +1,5 @@
 import mongoose, { Schema } from "mongoose";
 
-if (process.env.MONGODB_URI) {
-  mongoose.connect(process.env.MONGODB_URI);
-}
-
 const taskSchema = new Schema(
   {
     title: String,
--- app/api/Task/route.js
+++ app/api/Task/route.js
@@ -1,27 +1,40 @@
-import {NextResponse} from 'next/server';
-import Task from '../../(models)/Task'
+import { NextResponse } from "next/server";
+import Task from "../../(models)/Task";
+import { connectToDatabase } from "../../lib/mongoose";
 
 export async function POST(req) {
-	console.log('test')
-	try {
-		const body = await req.json()
-		const data = body.formData
-		const res = await Task.create(data)
-		if (res) {
-			return NextResponse.json( {message: 'Task Created Successfully!'}, {status: 201} )
-		}
-	}
-	catch ( error ) {
-		return NextResponse.json( {message: 'Failed to create', error}, {status: 500} )
-	}
+  console.log("Handling POST request");
+  try {
+    await connectToDatabase();
+    const body = await req.json();
+    const data = body.formData;
+    const res = await Task.create(data);
+    if (res) {
+      return NextResponse.json(
+        { message: "Task Created Successfully!" },
+        { status: 201 }
+      );
+    }
+  } catch (error) {
+    console.error("Error creating task:", error);
+    return NextResponse.json(
+      { message: "Failed to create", error },
+      { status: 500 }
+    );
+  }
 }
 
 export async function GET() {
-	try {
-		const task = await Task.find();
-		return NextResponse.json( {task}, {status: 200} )
-	}
-	catch ( error ) {
-		return NextResponse.json( {message: 'Failed to get task', error}, {status: 500} )
-	}
+  console.log("Handling GET request");
+  try {
+    await connectToDatabase();
+    const task = await Task.find();
+    return NextResponse.json({ task }, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching tasks:", error);
+    return NextResponse.json(
+      { message: "Failed to get task", error },
+      { status: 500 }
+    );
+  }
 }
