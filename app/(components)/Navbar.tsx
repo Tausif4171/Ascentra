6632712f8ca4d63bcea32e166719493bfeffe9fd
Tausif4171 <tausifkhan4171@gmail.com>
2024-08-31 12:11:40 +0530

    logout functionality added 🎉

--- app/(components)/Navbar.tsx
+++ app/(components)/Navbar.tsx
@@ -1,9 +1,21 @@
+"use client";
 import React from "react";
 import Icons from "../assets/svgs";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 function Navbar() {
   const { HomeIcon, TaskIcon, LogoutIcon } = Icons;
+  const token = localStorage.getItem("token");
+  const router = useRouter();
+
+  function handleLogout() {
+    localStorage.removeItem("token");
+
+    router.push("/auth/Login");
+    router.refresh();
+  }
+
   return (
     <div className=" bg-lime-100 flex justify-between py-4 px-10">
       <div className="flex gap-x-4">
@@ -22,9 +34,15 @@ function Navbar() {
         <LogoutIcon />
       </div> */}
 
-      <Link href={"/auth/Login"}>
-        <h4 className=" font-medium">Log in</h4>
-      </Link>
+      {token ? (
+        <h4 className="font-medium cursor-pointer" onClick={handleLogout}>
+          Log out
+        </h4>
+      ) : (
+        <Link href={"/auth/Login"}>
+          <h4 className="font-medium cursor-pointer">Log in</h4>
+        </Link>
+      )}
     </div>
   );
 }
